9b92dcadacc5109f27e8dce811f452a217bbf5d9
Cheng Vue <cvu3@ucsc.edu>
2018-11-20 08:54:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def batch_badge_creator(names_arr)
+  names_arr.collect do |ele|
+    "Hello, #{ele}!"
+  end
+end
+
+def assign_rooms
+  batch_badge_creator.collect do |ele|
+    "#{ele} You'll be assigned to room #{ele.index + 1}!"
+  end
+end
+
+def printer
+
+end
